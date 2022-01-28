7edb3b1effb354baa11fef18c82f6463899721e1
SantoshThakare <santoshthakare50@gmail.com>
2022-01-28 22:54:39 +0530

    [santosh]: Created folder

---
+++ SnakeAndLadderProblem/Snake_and_Laddder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class Snake_and_Laddder
+     { public void next ()
+     {
+            int NOPLAY = 0;
+     }
+    }
+}
